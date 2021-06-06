64de9cc8971e99ecd285ccca1c8041ff34c85712
jkopra01 <jkopra01@dcs.bbk.ac.uk>
2021-06-06 19:27:09 +0300

    get results from a day and slice to team and win

--- main.py
+++ main.py
@@ -2,15 +2,24 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-fromDate = '2021-01-13' #season start date
+seasonStartDate = '2021-01-13'
 toDate = '2021-04-13'
+fromDate = '2021-04-13'
 fromSeason = '20202021'
 toSeason = '20202021'
 
-url = "https://www.naturalstattrick.com/teamtable.php?fromseason=20202021&thruseason=20202021&stype=2&sit=5v5&score=all&rate=n&team=all&loc=B&gpf=410&fd=" + fromDate + "&td=" + toDate + "#"
+statisticUrl = "https://www.naturalstattrick.com/teamtable.php?fromseason=20202021&thruseason=20202021&stype=2&sit=5v5&score=all&rate=n&team=all&loc=B&gpf=410&fd=" + seasonStartDate + "&td=" + toDate + "#"
+resultsUrl = "https://www.naturalstattrick.com/teamtable.php?fromseason=20202021&thruseason=20202021&stype=2&sit=5v5&score=all&rate=n&team=all&loc=B&gpf=410&fd=" + fromDate + "&td=" + toDate
 
-df = pd.read_html(url, header=0, index_col = 0, na_values=["-"])[0]
+dfStats = pd.read_html(statisticUrl, header=0, index_col = 0, na_values=["-"])[0]
+dfResults = pd.read_html(resultsUrl, header=0, index_col = 0, na_values=["-"])[0]
+dfResults = pd.DataFrame(dfResults, columns = ['Team', 'W'])
+print(dfStats)
+print(dfResults)
 
-print(df)
+
+
 file_name = "test1.csv"
-df.to_csv(file_name, sep='\t', encoding='utf-8')
+dfStats.to_csv(file_name, sep='\t', encoding='utf-8')
+
+
