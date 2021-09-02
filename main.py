813d84078a82a5ecce6b8d326a1c62c6deeb0221
jkopra01 <jkopra01@dcs.bbk.ac.uk>
2021-09-02 23:17:34 +0300

    added tests

--- main.py
+++ main.py
@@ -35,7 +35,16 @@ SEASON_END_DATE_MONTH_2019_2020 = 3
 SEASON_START_DATE_YEAR_2019_2020 = 2019
 SEASON_END_DATE_YEAR_2019_2020 = 2020
 
+#Test dates
+SEASON_START_DATE_DAY_TEST = 28
+SEASON_START_DATE_MONTH_TEST = 12
+SEASON_END_DATE_DAY_TEST = 3
+SEASON_END_DATE_MONTH_TEST = 1
+SEASON_START_DATE_YEAR_TEST = 2019
+SEASON_END_DATE_YEAR_TEST = 2020
+
 l = []
+datesGathered = []
 
 def getSeasonAndMerge(SD, SM, SY, ED, EM, EY):
     seasonStartDate = str(SY) + "-0" + str(SM) + "-0" + str(SD)
@@ -50,7 +59,7 @@ def getSeasonAndMerge(SD, SM, SY, ED, EM, EY):
         toDate = str(SY) + "-" + str(SM) + "-" + str(SD)
         print(toDate)
         fromDate = toDate
-
+        datesGathered.append(toDate)
        #Stop from overflowing the server
         time.sleep(10)
         statisticUrl = "https://www.naturalstattrick.com/teamtable.php?fromseason="+fromSeason+"&thruseason="+toSeason+"&stype=2&sit=5v5&score=all&rate=n&team=all&loc=B&gpf=410&fd=" + seasonStartDate + "&td=" + toDate + "#"
@@ -62,7 +71,7 @@ def getSeasonAndMerge(SD, SM, SY, ED, EM, EY):
         dfResults = pd.DataFrame(dfResults, columns = ['Team', 'W'])
         mergedDataFrames = dfStats.merge(dfResults, how = 'inner', on = ['Team'])
         l.append(mergedDataFrames)
-
+        
         SD = int(SD)
         SM = int(SM)
         #february
@@ -85,10 +94,29 @@ def getSeasonAndMerge(SD, SM, SY, ED, EM, EY):
         else:
             SD+=1
 
-file_name = "nhlDataset.csv"
+file_name = "nhlDataset1.csv"
 getSeasonAndMerge(SEASON_START_DATE_DAY_2019_2020,SEASON_START_DATE_MONTH_2019_2020,SEASON_START_DATE_YEAR_2019_2020,SEASON_END_DATE_DAY_2019_2020,SEASON_END_DATE_MONTH_2019_2020,SEASON_END_DATE_YEAR_2019_2020)
 getSeasonAndMerge(SEASON_START_DATE_DAY_2018_2019,SEASON_START_DATE_MONTH_2018_2019,SEASON_START_DATE_YEAR_2018_2019,SEASON_END_DATE_DAY_2018_2019,SEASON_END_DATE_MONTH_2018_2019,SEASON_END_DATE_YEAR_2018_2019)
 getSeasonAndMerge(SEASON_START_DATE_DAY_2017_2018,SEASON_START_DATE_MONTH_2017_2018,SEASON_START_DATE_YEAR_2017_2018,SEASON_END_DATE_DAY_2017_2018,SEASON_END_DATE_MONTH_2017_2018,SEASON_END_DATE_YEAR_2017_2018)
 getSeasonAndMerge(SEASON_START_DATE_DAY_2016_2017,SEASON_START_DATE_MONTH_2016_2017,SEASON_START_DATE_YEAR_2016_2017,SEASON_END_DATE_DAY_2016_2017,SEASON_END_DATE_MONTH_2016_2017,SEASON_END_DATE_YEAR_2016_2017)
 
 pd.concat(l).to_csv(file_name, sep='\t', encoding='utf-8')
+
+
+
+import unittest
+import random
+class TestNotebook(unittest.TestCase):
+    def test_winColumnIsMerged(self):
+        seasonData = getSeasonAndMerge(SEASON_START_DATE_DAY_TEST,SEASON_START_DATE_MONTH_TEST,SEASON_START_DATE_YEAR_TEST,SEASON_END_DATE_DAY_TEST,SEASON_END_DATE_MONTH_TEST,SEASON_END_DATE_YEAR_TEST)
+        self.assertEqual(l[0].columns[l[0].columns.size-1], "W")    
+            
+    def test_dayMonthYearChangesCorrectly(self):
+        seasonData = getSeasonAndMerge(SEASON_START_DATE_DAY_TEST,SEASON_START_DATE_MONTH_TEST,SEASON_START_DATE_YEAR_TEST,SEASON_END_DATE_DAY_TEST,SEASON_END_DATE_MONTH_TEST,SEASON_END_DATE_YEAR_TEST)
+        self.assertIn("2020-01-01", datesGathered)
+
+    def test_dataFromAllDatesIsCollected(self):
+        dates = ["2019-12-28","2019-12-29","2019-12-30","2019-12-31","2020-01-01","2020-01-02"]
+        seasonData = getSeasonAndMerge(SEASON_START_DATE_DAY_TEST,SEASON_START_DATE_MONTH_TEST,SEASON_START_DATE_YEAR_TEST,SEASON_END_DATE_DAY_TEST,SEASON_END_DATE_MONTH_TEST,SEASON_END_DATE_YEAR_TEST)
+        self.assertIn(dates[random.randint(0, 5)], datesGathered) 
+unittest.main(argv=[''], verbosity=2, exit=False)
